8d148563aa4434166f5e68aed3c5344ec089c44d
Anton Malygin <ai.malygin@icloud.com>
2023-10-10 00:13:30 +0400

    Initial Commit

---
+++ Routing/RoutingApp.swift
@@ -0,0 +1,17 @@
+//
+//  RoutingApp.swift
+//  Routing
+//
+//  Created by Anton Malygin on 10.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct RoutingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
